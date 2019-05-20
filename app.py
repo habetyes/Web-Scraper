2fc9d152de3c7cd31dc4aad56016b58b68c36409
Habetyes <Habetyes@gmail.com>
2019-05-20 01:25:30 -0700

    corrected scraper paths

--- app.py
+++ app.py
@@ -1,20 +1,24 @@
 from flask import Flask, render_template, redirect
-from flask_pymongo import PyMongo
+import pymongo
 import scrape_mars
 
 # Create an instance of Flask
 app = Flask(__name__)
 
 # Use PyMongo to establish Mongo connection
-mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
+conn = 'mongodb://localhost:27017'
+client = pymongo.MongoClient(conn)
+db = client.mars
+collection = db.mars_info
 
+db.mars.drop()
 
 # Route to render index.html template using data from Mongo
 @app.route("/")
 def home():
 
     # Find one record of data from the mongo database
-    mars_data = mongo.db.collection.find_one()
+    mars_data = db.collection.find_one()
 
     # Return template and data
     return render_template("index.html", mars=mars_data)
@@ -28,7 +32,7 @@ def scrape():
     mars_data = scrape_mars.scrape()
 
     # Update the Mongo database using update and upsert=True
-    mongo.db.collection.update({}, mars_data, upsert=True)
+    db.collection.update({}, mars_data, upsert=True)
 
     # Redirect back to home page
     return redirect("/")
